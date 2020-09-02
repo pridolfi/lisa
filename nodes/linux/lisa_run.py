ad60934e2177e9b3011e88c3021bb6ab08f503cb
Pablo Ridolfi <pabloridolfi@gmail.com>
2020-09-02 01:38:56 -0300

    wip linux cli

--- nodes/linux/lisa_run.py
+++ nodes/linux/lisa_run.py
@@ -3,35 +3,44 @@
 LISA command line interface
 '''
 
+import os
 import sys
 import yaml
 from argparse import ArgumentParser
 
-def run_dispatcher():
+def run_dispatcher(options):
     from lisa.dispatcher import Dispatcher
     d = Dispatcher()
     d.run_dispatcher()
 
-def get_info():
+def get_info(options):
     from lisa.node import Node
     node = Node()
-    print(node.NODE_ID)
+    print(f'Node ID: {node.NODE_ID}')
     print(node.public_key.export_key().decode())
+    print(f'Node settings:')
     print(yaml.dump(node.settings, indent=4, sort_keys=True))
+
+def register_to_remote(options):
+    from lisa.node import Node
+    node = Node()
+    node.scp_exchange_pubkeys(options.uri)
 
 ACTIONS = {
     'dispatcher': run_dispatcher,
-    'info': get_info
+    'info': get_info,
+    'register': register_to_remote
 }
 
 def parse_arguments():
     parser = ArgumentParser(description='LISA command line interface')
-    parser.add_argument('action', nargs='?', help='Action to perform.', choices=ACTIONS.keys(), default='node')
+    parser.add_argument('action', nargs='?', help='Action to perform.', choices=ACTIONS.keys(), default='info')
+    parser.add_argument('--uri', '-u', help='Remote URI.', default=None)
     return parser.parse_args(sys.argv[1:])
 
 def main():
     options = parse_arguments()
-    ACTIONS[options.action]()
+    ACTIONS[options.action](options)
 
 if __name__ == "__main__":
     main()
