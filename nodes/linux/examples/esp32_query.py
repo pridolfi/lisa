cdd1826d7e3f06bf9ab10cf34d885273505f4fda
Pablo Ridolfi <pabloridolfi@gmail.com>
2020-10-29 23:39:37 -0300

    debug example

--- nodes/linux/examples/esp32_query.py
+++ nodes/linux/examples/esp32_query.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import time
+import logging
 from datetime import datetime
 from lisa.node import Node
 
 n = Node()
+n.logger.setLevel(logging.DEBUG)
 
 while True:
     try:
+        i = 0
         print('connecting...')
         n.start(wait_for_connection=True)        
         print('OK')
@@ -16,7 +19,7 @@ while True:
             devices = [dev for dev in devices if 'esp32' in dev]       
             for device in devices:
                 print(f'querying {device}...')
-                n.send_message(device, 'hello')
+                n.send_message(device, f'message {i}')
                 sender, message = n.recv_message(timeout_s=5)
                 if sender:
                     print(f'message from {sender}: {message}')
@@ -27,7 +30,7 @@ while True:
             while sender:
                 print(f'message from {sender}: {message}')
                 sender, message = n.recv_message(timeout_s=1)
-
+            i += 1
     except Exception as ex:
         print(f'exception {str(ex)}')
     except KeyboardInterrupt:
